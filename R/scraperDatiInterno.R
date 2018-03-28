5ef426a418e69cbac52aa1d6caef5f15298d2be0
Nico <nico.pro412@gmail.com>
2018-03-28 16:24:51 +0200

    upload version 1.1
    new version of datiMinInterno

---
+++ R/scraperDatiInterno.R
@@ -0,0 +1,16 @@
+scraperDatiInterno <-
+function(comuni, anno, quadro, posizione = NULL){
+  multidf <- list()
+  for (i in 1:length(comuni)){
+    for (j in 1:length(anno)){
+      for (k in 1:length(quadro)){
+        url <- paste0("http://finanzalocale.interno.it/apps/floc.php/certificati/index/codice_ente/",
+                      comuni[i],"/cod/4/anno/",anno[j],
+                      "/md/0/cod_modello/CCOU/tipo_modello/U/cod_quadro/", quadro[k])
+        url.table <- readHTMLTable(url, header = T, stringsAsFactors= F, which = posizione)
+       multidf[[i]] <- url.table
+      }
+    }
+  }
+  return (multidf)
+}
